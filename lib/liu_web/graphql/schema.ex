b3934e4f3925dee04359b06a4d63edf842f15c27
Oskar Legner <oskarlegner@gmail.com>
2021-03-16 13:38:35 +0100

    Hotfix

--- lib/liu_web/graphql/schema.ex
+++ lib/liu_web/graphql/schema.ex
@@ -8,58 +8,32 @@ defmodule LiuWeb.GraphQL.Schema do
   # Queries
   # - - - -
   query do
-    field :all_posts, list_of(:post) do
-      arg(:id, :id)
-
+    field :all_posts, list_of(:post_union) do
       resolve(&Resolver.list_posts/2)
     end
 
-    field :get_post, :post do
+    field :get_post, :post_union do
       arg(:id, :id)
 
       resolve(&Resolver.post_by_id/2)
     end
   end
 
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
   # Mutations
   # - - - -
   mutation do
     # ------------------------------------------------------
     # All in one
     # ------------------------------------------------------
-    #     field :create_post_all_in_one, :result do
-    #       arg(:title, non_null(:string))
-    #       arg(:type, non_null(:string))
-    #       arg(:content, :string)
-    #       arg(:resource_url, :string)
-    #       arg(:length, :integer)
-    #
-    #       resolve(&Resolver.create_post/2)
-    #     end
+    field :create_post_all_in_one, :result do
+      arg(:title, non_null(:string))
+      arg(:type, non_null(:string))
+      arg(:content, :string)
+      arg(:resource_url, :string)
+      arg(:length, :integer)
+
+      resolve(&Resolver.create_post/2)
+    end
 
     #
     #
@@ -69,13 +43,13 @@ defmodule LiuWeb.GraphQL.Schema do
     # ------------------------------------------------------
     # Tagged
     # ------------------------------------------------------
-    #     field :create_post_tagged, :result do
-    #       arg(:text_post, :tagged_text_post)
-    #       arg(:image_post, :tagged_image_post)
-    #       arg(:video_post, :tagged_video_post)
-    #
-    #       resolve(&Resolver.create_post/2)
-    #     end
+    field :create_post_tagged, :result do
+      arg(:text_post, :tagged_text_post)
+      arg(:image_post, :tagged_image_post)
+      arg(:video_post, :tagged_video_post)
+
+      resolve(&Resolver.create_post/2)
+    end
 
     #
     #
@@ -85,38 +59,38 @@ defmodule LiuWeb.GraphQL.Schema do
     # ------------------------------------------------------
     # Separate mutations
     # ------------------------------------------------------
-    #     field :create_text_post, :result do
-    #       arg(:title, :string)
-    #       arg(:content, :string)
-    #
-    #       resolve(fn params, _ ->
-    #         params
-    #         |> Map.put(:type, "text")
-    #         |> Resolver.create_post(nil)
-    #       end)
-    #     end
-    #
-    #     field :create_video_post, :result do
-    #       arg(:title, :string)
-    #       arg(:resource_url, :string)
-    #       arg(:length, :integer)
-    #
-    #       resolve(fn params, _ ->
-    #         params
-    #         |> Map.put(:type, "video")
-    #         |> Resolver.create_post(nil)
-    #       end)
-    #     end
-    #
-    #     field :create_image_post, :result do
-    #       arg(:title, :string)
-    #       arg(:resource_url, :string)
-    #
-    #       resolve(fn params, _ ->
-    #         params
-    #         |> Map.put(:type, "image")
-    #         |> Resolver.create_post(nil)
-    #       end)
-    #     end
+    field :create_text_post, :result do
+      arg(:title, :string)
+      arg(:content, :string)
+
+      resolve(fn params, _ ->
+        params
+        |> Map.put(:type, "text")
+        |> Resolver.create_post(nil)
+      end)
+    end
+
+    field :create_video_post, :result do
+      arg(:title, :string)
+      arg(:resource_url, :string)
+      arg(:length, :integer)
+
+      resolve(fn params, _ ->
+        params
+        |> Map.put(:type, "video")
+        |> Resolver.create_post(nil)
+      end)
+    end
+
+    field :create_image_post, :result do
+      arg(:title, :string)
+      arg(:resource_url, :string)
+
+      resolve(fn params, _ ->
+        params
+        |> Map.put(:type, "image")
+        |> Resolver.create_post(nil)
+      end)
+    end
   end
 end
--- lib/liu_web/graphql/types.ex
+++ lib/liu_web/graphql/types.ex
@@ -11,83 +11,50 @@ defmodule LiuWeb.GraphQL.Types do
     field(:length, :integer)
   end
 
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
   # ------------------------------------------------------
   # Union elements
   # ------------------------------------------------------
-  #
-  #   object :post_commons do
-  #     field(:id, :id)
-  #     field(:type, :string)
-  #     field(:title, :string)
-  #   end
-  #
-  #   # Post types
-  #
-  #   object :text_post do
-  #     field(:content, :string)
-  #
-  #     import_fields(:post_commons)
-  #   end
-  #
-  #   object :video_post do
-  #     field(:resource_url, :string)
-  #     field(:length, :integer)
-  #
-  #     import_fields(:post_commons)
-  #   end
-  #
-  #   object :image_post do
-  #     field(:resource_url, :string)
-  #
-  #     import_fields(:post_commons)
-  #   end
-  #
-  #   # Union
-  #
-  #   union :post_union do
-  #     types([
-  #       :text_post,
-  #       :image_post,
-  #       :video_post
-  #     ])
-  #
-  #     resolve_type(fn
-  #       %Post{type: "text"}, _ -> :text_post
-  #       %Post{type: "video"}, _ -> :video_post
-  #       %Post{type: "image"}, _ -> :image_post
-  #     end)
-  #   end
+
+  object :post_commons do
+    field(:id, :id)
+    field(:type, :string)
+    field(:title, :string)
+  end
+
+  # Post types
+
+  object :text_post do
+    field(:content, :string)
+
+    import_fields(:post_commons)
+  end
+
+  object :video_post do
+    field(:resource_url, :string)
+    field(:length, :integer)
+
+    import_fields(:post_commons)
+  end
+
+  object :image_post do
+    field(:resource_url, :string)
+
+    import_fields(:post_commons)
+  end
+
+  # Union
+
+  union :post_union do
+    types([
+      :text_post,
+      :image_post,
+      :video_post
+    ])
+
+    resolve_type(fn
+      %Post{type: "text"}, _ -> :text_post
+      %Post{type: "video"}, _ -> :video_post
+      %Post{type: "image"}, _ -> :image_post
+    end)
+  end
 end
